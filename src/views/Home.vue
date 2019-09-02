77827bafba88bdfdee009a3e7084439c76c92f07
KC <kckc@KCnoMacBook-Pro.local>
2019-09-02 21:41:10 +0900

    MOKデータ追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/basketball-ball-solid.svg">
     <HelloWorld msg="test"/>
+    
   </div>
+  
 </template>
-
+<i class="fas fa-basketball-ball"></i>
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an contact page</h1>
+  <div class="contact">
+    <h1>This is</h1>
   </div>
+  <div class="header-under">
+			<p class="hm-menu"><a href=""><img src="img/hm.png" alt=""></a></p>
+			<h1 class="main-logo"><a href=""><img src="img/header-logo.png" alt="logo"></a></h1>
+			<nav class="hd-sarch">
+					<ul>
+						<li><a href=""><img src="img/magnify.svg" alt=""></a></li>
+						<li><a href=""><img src="img/cart.svg" alt=""></a></li>
+					</ul>
+			</nav>
+		</div>
+  
+  
 </template>
+
