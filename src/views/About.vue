bbe17db20b66f2e3f835233a8fc912a6eac61f90
KC <kckc@KCnoMacBook-Pro.local>
2019-09-18 00:24:24 +0900

    html/scc更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,24 @@
-<template>
-  <div class="about">
-    <h1>This is an about page2
-    </h1>
+<div v-html="rawHtml"></div>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Hello</title>
+  <meta charset="UTF-8">
+  <script src="vue.js"></script>
+  <script src="main.js"></script>
+  <link rel="stylesheet" href="style.css"></link>
+</head>
+
+<body onload="initial();">
+  <h1>Vue.js</h1>
+  <div id="msg">
+    <p>{{ message }}</p>
+    <p v-html="message"></p>
+    <input type="text" v-model="text1">
+    <input type="button" v-on:click="doAction" value="click">
   </div>
-</template>
+</body>
+
+</html>
--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="contact">
-    <h1>This is</h1>
-  </div>
-  <div class="header-under">
-			<p class="hm-menu"><a href=""><img src="img/hm.png" alt=""></a></p>
-			<h1 class="main-logo"><a href=""><img src="img/header-logo.png" alt="logo"></a></h1>
-			<nav class="hd-sarch">
-					<ul>
-						<li><a href=""><img src="img/magnify.svg" alt=""></a></li>
-						<li><a href=""><img src="img/cart.svg" alt=""></a></li>
-					</ul>
-			</nav>
-		</div>
-  
-  
-</template>
-
+<html>
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  </head>
+  <body>
+    <div id="app">
+      <span v-html="msg"></span>
+    </div>
+  </body>
+</html>
+<script>
+  var obj = {
+    msg: '<span style="color:blue">HTMLコードとしてタグが判断され、青くなる</span>'
+  }
+ 
+  new Vue({
+    el: '#app',
+    data: obj
+  })
+</script>
